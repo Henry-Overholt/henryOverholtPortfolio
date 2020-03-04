eabfd46bf923e9f01fcb2c75c66e92910d0ab231
Henry <henrysoverholt@gmail.com>
2020-03-04 14:31:13 -0500

    built out contact form and contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"]
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  submitForm(form: NgForm): void {
+    let name: string = form.value.name;
+    let email: string = form.value.email;
+    let subject: string = form.value.subject;
+    let message: string = form.value.message;
+    console.log(`${name}, ${email}, ${subject}, ${message}`);
+    document.querySelector("form").reset();
   }
-
 }
