9ff9f9ff54dcb196551b14e52337dbe3881a9853
Henry <henrysoverholt@gmail.com>
2020-03-09 21:16:47 -0400

    added animations to nameon home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,24 @@ import {
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
   animations: [
+    trigger("fadeInName", [
+      state(
+        "void",
+        style({
+          opacity: 0
+        })
+      ),
+      transition("void=>*", animate(2000))
+    ]),
+    trigger("fadeInP", [
+      state(
+        "void",
+        style({
+          opacity: 0
+        })
+      ),
+      transition("void =>*", animate("1s 2000ms ease-in"))
+    ]),
     trigger("changePosition", [
       state(
         "initial",
@@ -22,85 +40,119 @@ import {
       state(
         "final",
         style({
-          left: "-100vw"
+          left: "-100vw",
+          background: "yellow"
         })
       ),
-      transition("initial=>final", animate("4000ms")),
-      transition("final=>initial", animate("4000ms"))
+      transition("initial=>final", animate("6000ms 500ms")),
+      transition("final=>initial", animate("6000ms 500ms"))
     ])
   ]
 })
 export class HomeComponent implements OnInit {
   currentState = "initial";
   bubbles: any[] = [
-    { class: "zero lg bubble left ", index: 0, state: "initial" },
-    { class: "one md bubble right", index: 1, state: "final" },
-    { class: "two sm bubble right", index: 2, state: "initial" },
-    { class: "three lg bubble left", index: 3, state: "final" },
-    { class: "four sm bubble right", index: 4, state: "initial" },
-    { class: "five md bubble left", index: 5, state: "final" },
-    { class: "six md bubble right", index: 6, state: "initial" },
-    { class: "seven lg bubble left", index: 7, state: "final" },
+    { class: " lg bubble  ", index: 0, state: "initial" },
+    { class: "md bubble ", index: 1, state: "final" },
+    { class: "md bubble ", index: 2, state: "initial" },
+    { class: "sm bubble ", index: 3, state: "final" },
+    { class: "sm bubble ", index: 4, state: "initial" },
+    { class: "sm bubble ", index: 5, state: "final" },
+    { class: "sm bubble ", index: 6, state: "initial" },
+    { class: "lg bubble ", index: 7, state: "final" },
     {
-      class: "eight sm bubble left",
+      class: " md bubble ",
       index: 8,
       state: "initial"
     },
-    { class: "nine sm bubble right", index: 9, state: "final" },
-    { class: "ten lg bubble left", index: 10, state: "initial" },
-    { class: "eleven md bubble right", index: 11, state: "final" }
+    { class: " lg bubble ", index: 9, state: "final" },
+    { class: " lg bubble ", index: 10, state: "initial" },
+    { class: " md bubble ", index: 11, state: "final" },
+    { class: " md bubble ", index: 12, state: "initial" },
+    { class: " sm bubble ", index: 13, state: "final" },
+    { class: "sm bubble", index: 14, state: "initial" }
   ];
-  randomInterval: number = Math.floor(Math.random() * (6000 - 4200) + 4200);
+  randomInterval: number;
 
   constructor() {}
 
   ngOnInit() {
-    this.staggerBubbles();
+    this.start();
   }
   start() {
+    this.nameIntro();
     this.staggerBubbles();
   }
   staggerBubbles() {
     this.changeState(0);
     setTimeout(() => {
       this.changeState(1);
-    }, 500);
+    }, Math.random() * (600 - 200) + 200);
     setTimeout(() => {
       this.changeState(2);
-    }, 100);
+    }, Math.random() * (1200 - 800) + 800);
     setTimeout(() => {
       this.changeState(3);
-    }, 1500);
+    }, Math.random() * (1700 - 1300) + 1300);
     setTimeout(() => {
       this.changeState(4);
-    }, 2000);
+    }, Math.random() * (2200 - 1800) + 1800);
     setTimeout(() => {
       this.changeState(5);
-    }, 2500);
+    }, Math.random() * (2700 - 2300) + 2300);
     setTimeout(() => {
       this.changeState(6);
-    }, 2500);
+    }, Math.random() * (3200 - 2700) + 2700);
+    //middle
     setTimeout(() => {
       this.changeState(7);
-    }, 2000);
+    }, Math.random() * (3700 - 3200) + 3200);
+    //middle
     setTimeout(() => {
       this.changeState(8);
-    }, 1500);
+    }, Math.random() * (3200 - 2700) + 2700);
     setTimeout(() => {
       this.changeState(9);
-    }, 1000);
+    }, Math.random() * (2700 - 2300) + 2300);
     setTimeout(() => {
       this.changeState(10);
-    }, 500);
-    this.changeState(11);
+    }, Math.random() * (2200 - 1800) + 1800);
+    setTimeout(() => {
+      this.changeState(11);
+    }, Math.random() * (1700 - 1300) + 1300);
+    setTimeout(() => {
+      this.changeState(12);
+    }, Math.random() * (1200 - 800) + 800);
+    setTimeout(() => {
+      this.changeState(13);
+    }, Math.random() * (600 - 200) + 200);
+    this.changeState(14);
   }
   changeState(i): void {
-    this.randomInterval = Math.floor(Math.random() * (6000 - 4200) + 4200);
+    this.randomInterval = Math.floor(Math.random() * (7500 - 6500) + 6500);
     this.bubbles[i].state =
       this.bubbles[i].state === "initial" ? "final" : "initial";
     setInterval(() => {
       this.bubbles[i].state =
         this.bubbles[i].state === "initial" ? "final" : "initial";
     }, this.randomInterval);
+  }
+  nameIntro() {
+    let title = document.querySelector("h1");
+    setTimeout(() => {
+      title.style.transition = "0.5s linear";
+      setTimeout(() => {
+        title.style.borderTop = "6px solid rgb(0, 0, 56)";
+      }, 400);
+      setTimeout(() => {
+        title.style.borderRight = "6px solid rgb(0, 0, 56)";
+      }, 500);
+      setTimeout(() => {
+        title.style.borderBottom = "6px solid rgb(0, 0, 56)";
+      }, 600);
+      setTimeout(() => {
+        title.style.borderLeft = "6px solid rgb(0, 0, 56)";
+      }, 700);
+    }, 1000);
   }
 }
