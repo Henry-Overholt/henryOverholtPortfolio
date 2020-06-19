d780fef3b1b54f4f274f676da54298759185ce09
Henry <henrysoverholt@gmail.com>
2020-06-19 17:36:42 -0400

    changed title of contact page

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -4,7 +4,7 @@ import {
   state,
   style,
   animate,
-  transition
+  transition,
 } from "@angular/animations";
 @Component({
   selector: "app-story",
@@ -13,13 +13,13 @@ import {
   animations: [
     trigger("fadeIn", [
       state("void", style({ opacity: 0 })),
-      transition("void=>*", animate("1000ms 100ms"))
+      transition("void=>*", animate("1000ms 100ms")),
     ]),
     trigger("fadeInLate", [
       state("void", style({ opacity: 0 })),
-      transition("void=>*", animate("1000ms 2s"))
-    ])
-  ]
+      transition("void=>*", animate("1000ms 2s")),
+    ]),
+  ],
 })
 export class StoryComponent implements OnInit {
   timeline: any[] = [
@@ -32,23 +32,23 @@ export class StoryComponent implements OnInit {
       strings: [
         "Grand Circus trains Michigan’s next generation of programmers and connects businesses to truly talented people.  Since 2015 they’ve graduated more than 1,500 bootcamp students, and have graduates at over 300 companies statewide. ",
         "I attended Grand Circus’ Frontend Bootcamp from July to September of 2019.  During that time I was taught frontend development from the ground up. Starting with recreating web layouts from screen shots using CSS and HTML, to making sites functional with JavaScript, and eventually learning the Angular Framework to build responsive efficient web applications.",
-        "While at Grand Circus I completed two to three mini projects a week, two mid-term projects, and graduated after completing my final project in a team of four using agile and scrum development principals."
+        "While at Grand Circus I completed two to three mini projects a week, two mid-term projects, and graduated after completing my final project in a team of four using agile and scrum development principals.",
       ],
-      index: 0
+      index: 0,
     },
     {
       logo: "./../../assets/careerTimeline/forOurFutureLogo.png",
       company: " ~ For Our Future MI",
       alt: "For Our Future MI Logo",
       position: "Field Organizer",
-      date: "September 2019 - December 2018",
+      date: "September 2018 - December 2018",
       strings: [
         "For Our Future is a Political Action Committee, and I worked for the non-profit arm.  The goal of the non-profit arm of For Our Future is to go out a listen to Michigan Voters, and find out what the most important issues were for each individual we talked to. ",
         "The goal of my position was voter engagement.  My main responsibility was to reach as many voters as I could through door-to-door contact.  I engaged voters in a conversation to figure out what issues were most important to them.  For Our Future would take that information and support local candidates who matched the issues of the community they were running to represent. ",
         "When we got closer to the election, my goal was to encourage them to vote, and vote for candidates whose platform matched the issues they cared about.  Throughout my time with For Our Future during the 2018 elections, I knocked on over two thousand doors, and talked to people about their vote and the power they carried, to elect the candidates who best represented their issues.",
-        "After the election we mobilized to get voters to call their current representatives in the Michigan house and senate, to protect certain legislation that was on the floor to be repealed by representatives who were voted out.  I was calling, knocking on doors, and going to events to talk to as many voters as possible."
+        "After the election we mobilized to get voters to call their current representatives in the Michigan house and senate, to protect certain legislation that was on the floor to be repealed by representatives who were voted out.  I was calling, knocking on doors, and going to events to talk to as many voters as possible.",
       ],
-      index: 1
+      index: 1,
     },
     {
       logo: "./../../assets/careerTimeline/saadCongressLogo.png",
@@ -58,9 +58,9 @@ export class StoryComponent implements OnInit {
       date: "July 2018 - August 2018",
       strings: [
         "THis was a campiagn for local offices",
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?"
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?",
       ],
-      index: 2
+      index: 2,
     },
     {
       logo: "./../../assets/careerTimeline/cityyearLogo.png",
@@ -70,9 +70,9 @@ export class StoryComponent implements OnInit {
       date: "July 2017 - June 2018",
       strings: [
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?",
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?"
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?",
       ],
-      index: 3
+      index: 3,
     },
     {
       logo: "./../../assets/careerTimeline/cityyearLogo.png",
@@ -82,9 +82,9 @@ export class StoryComponent implements OnInit {
       date: "August 2016 - June 2017",
       strings: [
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?",
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?"
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?",
       ],
-      index: 4
+      index: 4,
     },
     {
       logo: "./../../assets/careerTimeline/ohioLogo.png",
@@ -94,10 +94,10 @@ export class StoryComponent implements OnInit {
       date: "August 2012 - August 2016",
       strings: [
         "This is my most recent experience",
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?"
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores accusamus, rerum quisquam, accusantium cupiditate nulla ipsum incidunt pariatur consequatur animi architecto cumque neque hic quo inventore laudantium totam quia reiciendis?",
       ],
-      index: 5
-    }
+      index: 5,
+    },
   ];
   currentTimeline: any = this.timeline[0];
   lastTimeLine: any = this.timeline[0];
@@ -109,7 +109,7 @@ export class StoryComponent implements OnInit {
     { number: 2 },
     { number: 3 },
     { number: 4 },
-    { number: 5 }
+    { number: 5 },
   ];
   constructor() {}
 
