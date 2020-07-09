7f9e2c14cc1048f51bb8851adfd5922167b3bfac
Henry <henrysoverholt@gmail.com>
2020-07-09 13:52:29 -0400

    added TA position to story

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -27,6 +27,15 @@ export class StoryComponent implements OnInit {
       logo: "./../../assets/careerTimeline/gcLogo.png",
       company: " ~ Grand Circus",
       alt: "Grand Circus Bootcamp Logo",
+      position: "After Hours Bootcamp Teaching Assistant",
+      date: "July 2020 - Present",
+      strings: ["", "", ""],
+      index: 0,
+    },
+    {
+      logo: "./../../assets/careerTimeline/gcLogo.png",
+      company: " ~ Grand Circus",
+      alt: "Grand Circus Bootcamp Logo",
       position: "Frontend Bootcamp",
       date: "July 2019 - September 2019",
       strings: [
@@ -34,7 +43,7 @@ export class StoryComponent implements OnInit {
         "I attended Grand Circus’ Frontend Bootcamp from July to September of 2019.  During that time I was taught frontend development from the ground up. Starting with recreating web layouts from screen shots using CSS and HTML, to making sites functional with JavaScript, and eventually learning the Angular Framework to build responsive efficient web applications.",
         "While at Grand Circus I completed two to three mini projects a week, two mid-term projects, and graduated after completing my final project in a team of four using agile and scrum development principals.",
       ],
-      index: 0,
+      index: 1,
     },
     {
       logo: "./../../assets/careerTimeline/forOurFutureLogo.png",
@@ -48,7 +57,7 @@ export class StoryComponent implements OnInit {
         "As we got closer to the election, my goal was to encourage them to vote, and vote for candidates whose platform matched the issues they cared about.  Throughout my time with For Our Future during the 2018 elections, I knocked on over two thousand doors, and talked to people about their vote and the power they carried, to elect the candidates who best represented their issues.",
         "After the election we mobilized to get voters to call their current representatives in the Michigan house and senate, to protect certain legislation that was on the floor to be repealed by representatives who were voted out.  I was calling, knocking on doors, and going to events to talk to as many voters as possible.",
       ],
-      index: 1,
+      index: 2,
     },
     {
       logo: "./../../assets/careerTimeline/saadCongressLogo.png",
@@ -61,7 +70,7 @@ export class StoryComponent implements OnInit {
         "Throughout my time with the Saad for Congress Campaign I represented the campaign and it’s message at local events, meetings, and community festivals throughout the 11th District.  I created training documents for new volunteers as well as ran training sessions.  In the closing phases of the campaign, to get our supporters out and vote, I managed 150 “Get Out the Vote” shifts, launching canvasses as well as phone banks.",
         "My time with the campaign was a true highlight of my young career at that point.  The people I worked with were amazing.  The pace at which we worked, and amount we got done in the day left me feeling truly inspired.  When I’ve been looking for projects since my time with the Saad for Congress campaign, I look for the same things I had there, a great collaborative environment, solution oriented peers, and shared values that help us get the job done. ",
       ],
-      index: 2,
+      index: 3,
     },
     {
       logo: "./../../assets/careerTimeline/cityyearLogo.png",
@@ -74,7 +83,7 @@ export class StoryComponent implements OnInit {
         "City Year places young adults, ages 18-24, inside schools for a whole year. They partner with a teacher to focus on an assigned list of students, in order to tutor, mentor, and coach students. As the Team Leader, alongside my Manager, I was the go-between for our team, and the administration at our school. I build relationships with 30+ faculty members in order to achieve our teams goals. I also collected, entered, and managed our teams data on 120+ students. ",
         "During my second year with City Year Detroit, I designed an afterschool program for 20 students, with support from Learn Fresh and the Detroit Pistons, where they used the sport of basketball and their favorite NBA and WNBA players to improve their math skills. I also led a computer programming course for third thru sixth graders to introduce them to very basic programming concepts like if/else statements.  Finally I created and implemented a school wide reading challenge, where the school surpassed the initial goal of reading 23,000 minutes, instead read almost 49,000 minutes. ",
       ],
-      index: 3,
+      index: 4,
     },
     {
       logo: "./../../assets/careerTimeline/cityyearLogo.png",
@@ -88,7 +97,7 @@ export class StoryComponent implements OnInit {
         "My role in our team was as the Attendance Coordinator.  Meaning I was in charge of planning, organizing, and executing school-wide initiatives that encouraged and rewarded good attendance.  During my time we created the 95% club, where students who held a 95% attendance for the month, was allowed to attend movie and popcorn parties, hot chocolate parties, as well as open gym times.  As attendance coordinator, I had access to little resources, so materials and snacks needed to be obtained through donations that I would reach out to local organizations to receive.",
         "City Year Detroit brought me from Ohio to Michigan, but it was the people and the city that encourage me to stay, and I’ve loved it ever since.",
       ],
-      index: 4,
+      index: 5,
     },
   ];
   courses: any[] = [
@@ -106,7 +115,7 @@ export class StoryComponent implements OnInit {
       date: "August '12 - August '16",
       location: "Athens, OH",
       logo: "./../../assets/careerTimeline/ohioLogo.png",
-      pictureAlt: "Ohio Unversity Logo",
+      pictureAlt: "Ohio University Logo",
     },
   ];
   currentTimeline: any = this.timeline[0];
@@ -119,7 +128,7 @@ export class StoryComponent implements OnInit {
     { number: 2 },
     { number: 3 },
     { number: 4 },
-    // { number: 5 },
+    { number: 5 },
   ];
   constructor() {}
 
