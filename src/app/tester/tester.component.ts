c4bc74f379c02ae38cb845a1266e749911eaee21
Henry Overholt <henrysoverholt@gmail.com>
2020-09-07 19:50:30 -0400

    added animations to contact component

--- src/app/tester/tester.component.ts
+++ src/app/tester/tester.component.ts
@@ -6,40 +6,10 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./tester.component.css"],
 })
 export class TesterComponent implements OnInit {
-  squares: any[] = [
-    { id: "zero", show: false, class: "box " },
-    { id: "one", show: false, class: "box " },
-    { id: "two", show: false, class: "box " },
-    { id: "three", show: false, class: "box" },
-    { id: "four", show: false, class: "box" },
-    { id: "five", show: false, class: "box " },
-    { id: "six", show: false, class: "box " },
-    { id: "seven", show: false, class: "box" },
-    { id: "eight", show: false, class: "box" },
-    { id: "nine", show: false, class: "box " },
-    { id: "ten", show: false, class: "box " },
-    { id: "eleven", show: false, class: "box  " },
-    { id: "twelve", show: false, class: "box" },
-    { id: "thirteen", show: false, class: "box  " },
-    { id: "fourteen", show: false, class: "box" },
-    { id: "fifteen", show: false, class: "box " },
-  ];
+  squares: any[];
   constructor() {}
 
   ngOnInit(): void {
-    this.runBars();
-  }
-  runBars() {
-    setTimeout(() => {
-      setTimeout(() => {
-        document.getElementById("first").style.width = "80%";
-      }, 200);
-      setTimeout(() => {
-        document.getElementById("second").style.width = "60%";
-      }, 600);
-      setTimeout(() => {
-        document.getElementById("third").style.width = "40%";
-      }, 1000);
-    }, 500);
+    // this.runBars();
   }
 }
