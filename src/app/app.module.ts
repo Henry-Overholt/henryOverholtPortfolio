d7b0daec56496b58bd7b27bddae9916412db15be
Henry Overholt <henrysoverholt@gmail.com>
2020-12-15 21:21:14 -0500

    added side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,16 @@ import { ProjectComponent } from "./projects/project/project.component";
 import { FooterComponent } from "./footer/footer.component";
 import { TimelineComponent } from "./story/timeline/timeline.component";
 import { HeaderComponent } from "./header/header.component";
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from "./button/button.component";
+import { BlogComponent } from "./blog/blog.component";
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 const appRoutes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "story", component: StoryComponent },
   { path: "projects", component: ProjectsComponent },
   { path: "contact", component: ContactComponent },
+  { path: "blog", component: BlogComponent },
   { path: "**", component: HomeComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
 ];
@@ -36,6 +39,8 @@ const appRoutes: Routes = [
     TimelineComponent,
     HeaderComponent,
     ButtonComponent,
+    BlogComponent,
+    SideNavComponent,
   ],
   imports: [
     BrowserModule,
